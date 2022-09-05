40143883c27d7b9c1e6939054818b8c6417e0960
Tatiana Minenko <ta.minenko@ya.ru>
2022-09-05 09:58:50 +0300

    Проект спринта 9: правки (1)

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -3,7 +3,7 @@ from rest_framework import filters, viewsets
 from rest_framework.pagination import LimitOffsetPagination
 from rest_framework.permissions import IsAuthenticated
 
-from posts.models import Follow, Group, Post
+from posts.models import Group, Post
 from .mixins import ListCreateViewSet
 from .permissions import IsAuthorOrReadOnly, ReadOnly
 from .serializers import (
